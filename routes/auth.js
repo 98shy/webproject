42c7628b84c8d12d0023db5935fdd95d93919b48
Yoo <sanghyun425@naver.com>
2019-12-15 04:38:28 +0900

    commit

--- routes/auth.js
+++ routes/auth.js
@@ -23,6 +23,20 @@ module.exports = (app, passport) => {
     }
   );
 
+  app.get('/auth/naver',
+    passport.authenticate('naver', { scope : 'email' })
+  );
+
+  app.get('/auth/naver/callback',
+    passport.authenticate('naver', {
+      failureRedirect : '/signin',
+      failureFlash : true // allow flash messages
+    }), (req, res, next) => {
+      req.flash('success', '환영합니다!');
+      res.redirect('/');
+    }
+  );
+
   app.get('/signout', (req, res) => {
     req.logout();
     req.flash('success', '성공적으로 로그아웃 됐습니다.');
